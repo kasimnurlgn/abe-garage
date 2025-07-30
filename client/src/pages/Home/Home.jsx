73705e4289986a225dddfc93e5d3f3bdbee27cf3
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-07-30 17:55:19 +0300

    Folder structure modified

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Hero from "../../components/Hero/Hero";
+import About from "../../components/About/About";
+import Services from "../../components/Services/Services";
+import Features from "../../components/Features/Features";
+import WhyUs from "../../components/WhyUs/WhyUs";
+import Since from "../../components/Since/Since";
+import Layout from "../Layout/Layout";
+import Appointment from "../../components/Appointment/Appointment";
+function Home() {
+  return (
+    <Layout>
+      <Hero />
+      <About />
+      <Services />
+      <Features />
+      <WhyUs />
+      <Since />
+      <Appointment />
+    </Layout>
+  );
+}
+
+export default Home;
