14a77204cdebdfdeef5b410158765949129279cb
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-08-02 14:50:25 +0300

    Contact page done

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ContactUs from "../../components/ContactUs/ContactUs";
 import Layout from "../../pages/Layout/Layout";
+import Appointment from "../../components/Appointment/Appointment";
 function Contact() {
   return (
     <Layout>
       <ContactUs />
+      <Appointment />
     </Layout>
   );
 }
