87be6b0af6b1f52ef9ca79ed23930daa1746a263
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-08-02 21:40:45 +0300

    Scrolling effect added

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -4,18 +4,22 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Service from "./pages/Services/Services";
 import Contact from "./pages/Contact/Contact";
-import Login from "./pages/Login/Login"
+import Login from "./pages/Login/Login";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 function RouterApp() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Service />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/login" element={<Login />} />
-      {/* Redirect to Home for unmatched routes */}
-      {/* Add more routes as needed */}
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        {/* Redirect to Home for unmatched routes */}
+        {/* Add more routes as needed */}
+      </Routes>
+    </>
   );
 }
 
